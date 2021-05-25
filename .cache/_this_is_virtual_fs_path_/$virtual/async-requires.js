6d4d627ff35e4e3836d16b7ed9a6eb80752a2045
lukaszSu <lukasz.skiba@fireart-d.com>
2021-05-25 10:23:47 +0200

    feedback

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-template-blog-js": () => import("./../../../src/template/blog.js" /* webpackChunkName: "component---src-template-blog-js" */),
   "component---src-template-post-js": () => import("./../../../src/template/post.js" /* webpackChunkName: "component---src-template-post-js" */)
